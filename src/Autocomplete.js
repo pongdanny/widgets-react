3650649c483907cdd5e68f0f3746464e0369440b
pongdanny <pongdaniel17@gmail.com>
2020-11-15 13:37:52 -0500

    finish weather widget

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,13 +1,48 @@
-import React from 'react';
-
+import React from "react";
 
 class Weather extends React.Component {
-  constructor () {
+  constructor() {
     super();
 
     this.state = {
       weather: "Loading...",
-      city: "Loading..."
+      city: "Loading...",
     };
-
   }
+
+  pollWeather = async ({ coords }) => {
+    const apikey = "insertAPIKeyHere";
+    const { latitude, longitude } = coords;
+    try {
+      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      this.setState({
+        weather: Math.floor((data.main.temp - 273) * (9 / 5) + 32),
+        city: data.name,
+      });
+    } catch {
+      this.setState({
+        weather: "Unknown",
+        city: "Unknown",
+      });
+    }
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.pollWeather);
+  }
+
+  render() {
+    return (
+      <div className="weather-box">
+        <h1 className="widget-header">Weather</h1>
+        <div className="weather-city">
+          <div>{`${this.state.weather}°`}</div>
+          <div>{`${this.state.city}`}</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Weather;
