5160d6678d4d95c195cc0da0a4de018a038241e1
pongdanny <pongdaniel17@gmail.com>
2020-11-15 13:40:03 -0500

    setup autocomplete class component

---
+++ src/Autocomplete.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import {TransitionGroup} from 'react-transition-group';
+
+class Autocomplete extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputVal: "",
+    };
+  }
+
+
+
+export default Autocomplete;
