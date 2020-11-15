7a892d5985a2d830b65de45dd0aedc95668fc6bf
pongdanny <pongdaniel17@gmail.com>
2020-11-15 13:40:40 -0500

    finish autocomplete class component

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -9,6 +9,50 @@ class Autocomplete extends React.Component {
     };
   }
 
+  handleInput = (e) => {
+    this.setState({
+      inputVal: e.target.value,
+    });
+  };
 
+  updateInput = (e) => {
+    this.setState({
+      inputVal: e.target.value,
+    });
+  };
+
+  matches = () => {
+    if (this.state.inputVal === "") {
+      return this.props.names;
+    }
+    let res = this.props.names.filter((name) =>
+      name.toLowerCase().includes(this.state.inputVal.toLowerCase())
+    );
+    return res;
+  };
+
+  render() {
+    return (
+      <div className="auto-box-roll-out">
+        <h1 className="widget-header">Autocomplete</h1>
+        <div className="auto-box-content">
+          <input
+            onChange={this.handleInput}
+            value={this.state.inputVal}
+            className="auto-input"
+            type="text"
+          />
+          <ul className="item-list" onClick={this.updateInput}>
+            {/* <CSSTransition> */}
+            {this.matches().map((name, idx) => (
+              <li key={idx}>{name}</li>
+            ))}
+            {/* </CSSTransition> */}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Autocomplete;
